9a539a9d774abc3623aa7345f62eb9918b0b2aee
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2024-09-28 16:16:52 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Start QEMU in the background
-qemu-system-x86_64 -m 256 -boot d -cdrom /qemu/images/linux.iso \
-    #-drive file=/qemu/images/disk.qcow2,format=qcow2 -vnc :0 &
+qemu-system-x86_64 -m 256 -boot d -cdrom /qemu/images/linux.iso -vnc :0 &
+# -drive file=/qemu/images/disk.qcow2,format=qcow2
 
 # Start noVNC
 /opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080
