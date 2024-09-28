4d9b013ed792d8cbf35a287e7528a56c5f38203a
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2024-09-28 16:50:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start QEMU in the background
-qemu-system-x86_64 -m 256 -boot d -cdrom /qemu/images/linux.iso -vnc :0 &
+qemu-system-x86_64 -m 256 -boot d -cdrom "/qemu/images/Hiren's.BootCD.15.2.iso" -device qxl -cpu host -accel kvm -M pc -netdev user,id=lan -device rtl8139,netdev=lan -usb -device usb-tablet -rtc base=localtime -monitor stdio -vnc :0 &
 # -drive file=/qemu/images/disk.qcow2,format=qcow2
 
 # Start noVNC
