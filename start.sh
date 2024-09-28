a2334d88b6ee368c4890b6cf5cde4628920d0884
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2024-09-28 17:42:27 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start QEMU in the background
-qemu-system-x86_64 -m 256 -boot d -cdrom /qemu/images/hirensbootcd.iso -device qxl -cpu core2duo,+sse3,+sse4.1,+sse4.2 -smp cores=2 -accel tcg -M pc -netdev user,id=lan -device rtl8139,netdev=lan -usb -device usb-tablet -rtc base=localtime -monitor stdio -vnc :0 &
+qemu-system-x86_64 -m 256 -boot d -cdrom /qemu/images/hirensbootcd.iso -device qxl -cpu core2duo,+sse3,+sse4.1,+sse4.2 -accel tcg -M pc -netdev user,id=lan -device rtl8139,netdev=lan -usb -device usb-tablet -rtc base=localtime -monitor stdio -vnc :0 &
 # -drive file=/qemu/images/disk.qcow2,format=qcow2
 
 # Start noVNC
